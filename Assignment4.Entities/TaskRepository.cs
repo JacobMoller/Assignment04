f43ba8af74acfb859b47af11d704b3da9a0813d1
JacobM <jacj@itu.dk>
2021-10-04 17:05:36 +0200

    Method signatures for TaskRepository

--- Assignment4.Entities/TaskRepository.cs
+++ Assignment4.Entities/TaskRepository.cs
@@ -1,6 +1,41 @@
+using System;
+using Assignment4.Core;
+using System.Collections.Generic;
+
 namespace Assignment4.Entities
 {
-    public class TaskRepository
+    public class TaskRepository : ITaskRepository
     {
+        public IReadOnlyCollection<TaskDTO> All()
+        {
+            throw new NotImplementedException();
+            //SELECT * FROM Tasks
+        }
+        public int Create(TaskDTO task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaskDetailsDTO FindById(int id)
+        {
+            throw new NotImplementedException();
+            //SELECT * FROM Tasks WHERE taskId = id
+            //^Remember to avoid sql injections here
+        }
+
+        public void Update(TaskDTO task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
